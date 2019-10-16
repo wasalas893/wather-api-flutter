18a82b4391dc9cbc1687446183e5ffd4bb5b1019
wasalas <wasalas893@gmail.com>
2019-10-16 17:21:24 +0530

    using dart

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './ui/klimatic.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Klimatic',
+      home: new Klimatic(),
+
+    )
+
+  );
+}
